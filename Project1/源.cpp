03014e33ffa0613bfca8df529f914a0014acdad0
aoikuma <48854115+BluebearOfficial@users.noreply.github.com>
2022-10-26 14:33:16 +0800

    Add files via upload

---
+++ Project1/源.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void main()
+{
+	int a, b, sum;
+	cout << sizeof(int) << endl;
+	cout << sizeof(long int) << endl;
+	cout << sizeof(short int) << endl;
+	cout << sizeof(long) << endl;
+	cout << sizeof(short) << endl;
+
+
+}
